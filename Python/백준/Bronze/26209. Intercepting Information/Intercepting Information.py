bc4a52df772b125529971364f2c4c2d2c45138a7
Adeline Oh <skanmim12@gmail.com>
2026-03-18 14:46:32 +0900

    [Bronze V] Title: Intercepting Information, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/26209. Intercepting Information/Intercepting Information.py
@@ -0,0 +1,5 @@
+N1, N2, N3, N4, N5, N6, N7, N8 = map(int, input().split())
+
+if (N1 == 0 or N1 == 1) and (N2 == 0 or N2 == 1) and (N3 == 0 or N3 == 1) and (N4 == 0 or N4 == 1) and (N5 == 0 or N5 == 1) and (N6 == 0 or N6 == 1) and (N7 == 0 or N7 == 1) and (N8 == 0 or N8 == 1):
+    print("S")
+else:    print("F")   
